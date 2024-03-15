c8c29292fef9c54150ac5bb0f7514edb8ed883e4
Ilyas EZZALMATI <ilyas.ezzalmati@ensiie.eu>
2024-03-15 15:54:19 +0000

    tache A.2

--- include/board.h
+++ include/board.h
@@ -2,7 +2,7 @@
 #define __board_h__
 #include "tetromino.h"
 
-typedef void *board;
+typedef BoardStruct *board;
 
 /**
  * Signature create_board
@@ -21,60 +21,60 @@ void free_board(board b);
 
 /**
  * Signature list_tetrominos_in_bag
- * @param b Le board dont on veut la liste des tétrominos dans le sac.
- * @return Un tableau de tétrominos présents dans le sac.
+ * @param b Le board dont on veut la liste des tétraminos dans le sac.
+ * @return Un tableau de tétraminos présents dans le sac.
  */
 tetromino *list_tetrominos_in_bag(board b);
 
 /**
  * Signature add_tetromino_to_bag
- * @param b Le board auquel ajouter le tétromino dans le sac.
- * @param t Le tétromino à ajouter.
+ * @param b Le board auquel ajouter le tétraminos dans le sac.
+ * @param t Le tétraminos à ajouter.
  */
 void add_tetromino_to_bag(board b, tetromino t);
 
 /**
  * Signature remove_tetromino_from_bag
- * @param b Le board duquel retirer le tétromino du sac.
- * @param t Le tétromino à retirer.
+ * @param b Le board duquel retirer le tétraminos du sac.
+ * @param t Le tétraminos à retirer.
  */
 void remove_tetromino_from_bag(board b, tetromino t);
 
 /**
  * Signature check_place_tetromino
- * @param b Le board sur lequel vérifier la possibilité de placer le tétromino.
+ * @param b Le board sur lequel vérifier la possibilité de placer le tétraminos.
  * @param r La ligne de référence.
  * @param c La colonne de référence.
- * @param t Le tétrominos à placer.
- * @return 1 si le tétromino peut être placé, 0 sinon.
+ * @param t Le tétraminos à placer.
+ * @return 1 si le tétraminos peut être placé, 0 sinon.
  */
 int check_place_tetromino(board b, int r, int c, tetromino t);
 
 /**
  * Signature place_tetromino
- * @param b Le board sur lequel placer le tétromino.
+ * @param b Le board sur lequel placer le tétraminos.
  * @param r La ligne de référence.
  * @param c La colonne de référence.
- * @param t Le tétromino à placer.
- * @return 1 si le tétromino a été placé avec succès, 0 sinon.
+ * @param t Le tétraminos à placer.
+ * @return 1 si le tétraaddminos a été placé avec succès, 0 sinon.
  */
 int place_tetromino(board b, int r, int c, tetromino t);
 
 /**
  * Signature remove_tetromino
- * @param b Le board duquel retirer le tétromino.
+ * @param b Le board duquel retirer le tétraminos.
  * @param r Pointeur vers la ligne de référence.
  * @param c Pointeur vers la colonne de référence.
- * @param t Le tétromino à retirer.
+ * @param t Le tétraminos à retirer.
  */
 void remove_tetromino(board b, int *r, int *c, tetromino t);
 
 /**
  * Signature get_tetromino
- * @param b Le board sur lequel chercher le tétromino.
+ * @param b Le board sur lequel chercher le tétraminos.
  * @param r La ligne de référence.
  * @param c La colonne de référence.
- * @return Le tétromino trouvé ou NULL s'il n'y en a pas.
+ * @return Le tétraminos trouvé ou NULL s'il n'y en a pas.
  */
 tetromino get_tetromino(board b, int r, int c);
 
