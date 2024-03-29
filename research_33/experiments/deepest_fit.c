1dfeef00e2762f47294dfa8c72403e394f23ab79
Nael GUERARD <nael.guerard@pc225-21.pedago.ensiie.fr>
2024-03-29 17:27:13 +0100

    Tache D.1 : modif le .h et .c de deepest_fit

--- research_33/experiments/deepest_fit.c
+++ research_33/experiments/deepest_fit.c
@@ -1,10 +1,48 @@
 #include "../../include/board.h"
+#include "deepest_fit.h"
 
+#define IMPLACABLE 1000
 
-int max_hauteur_tetromino(board b, tetromino t){
-    for(int i=0;i<)
+int* min_hauteur_tetromino(board b, tetromino t){
+    int r=0;   //ligne
+    int c=0;   //colonne
+    int tab[2];
+    while (!check_place_tetromino(b ,r, c, t)) {  //tant qu'on n'a pas trouvé la case la plus en haut
+        if(c==nbColonnes(b)-1){           //arriver en bout de ligne
+            c=0;
+            r++;
+            if(r==get_nbLignes(b)){        //quand la grille a été totalement parcourue
+                tab[0]=IMPLACABLE;
+                tab[1]=IMPLACABLE;
+                return tab;
+            }
+        }
+        else{
+            c++;
+        }
+    }
+    tab[0]=r;
+    tab[1]=c;
+    return tab;
 }
 
-void deepest_fit(board b){
-
+int* deepest_fit(board b){
+    int r=IMPLACABLE;
+    int indice=-1;
+    int valeurs[3];
+    tetromino* sac=sac(b);
+    for(int i=0; i<tailleSac(b); i++){
+        if(r>=min_hauteur_tetromino(b, sac[i])[0]){
+            r=min_hauteur_tetromino(b, sac[i])[0];
+            indice=i;
+        }
+    }
+    if (place_tetromino(b, r, c, sac[indice])) {  
+        valeurs[0]=min_hauteur_tetromino(b, sac[indice])[0];
+        valeurs[1]=min_hauteur_tetromino(b, sac[indice])[1];
+        valeurs[2]=indice;
+    }
+    else {
+        return 
+    }
 }
--- research_33/experiments/deepest_fit.h
+++ research_33/experiments/deepest_fit.h
@@ -2,7 +2,12 @@
 #define __DEEPEST_FIT__
 #include "../../include/board.h"
 
-void deepest_fit(board b);
+/* @requires : board b and tetromino t
+   @asssigns : nothing
+   @ensures :  returning the coordinates of the highest possible place for the tetromino*/
+int* min_hauteur_tetromino(board b, tetromino t)
+
+int* deepest_fit(board b);
 
 
 
