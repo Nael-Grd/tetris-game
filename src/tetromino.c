9780e6768047e0da52eca11158ef64e6ba80609d
Ilyas EZZALMATI <ilyas.ezzalmati@ensiie.eu>
2024-05-19 21:55:07 +0000

    tache E.2 [tetromino.c]

--- src/tetromino.c
+++ src/tetromino.c
@@ -193,4 +193,48 @@ int* get_tetromino_cells(tetromino tr) {
         }
     }
     return cells;
+
+    void display_tetromino(tetromino t){ // ceci affiche le tetromino
+    int taille=6;
+    // créer la mini grille qui est de 6 lignes et 6 colonnes
+    char** mini_grille=malloc(taille*sizeof(char*));
+    if(mini_grille==NULL){
+        printf("erreur d'allocation de la mini grille\n");
+        exit(8);
+    }
+    for (int i=0;i<taille;i++){
+        mini_grille[i]=malloc(taille*sizeof(char));
+        if(mini_grille[i]==NULL){
+            printf("erreur d'allocation de la mini grille %d\n",i);
+            exit(9+i);
+        }
+    }
+    for(int h=0; h<taille;h++){
+        for(int k=0; k<taille; k++){
+            mini_grille[h][k]='-';
+        }
+    }
+    // la remplir par le tetromino: on commence par la case à la 5eme ligne et la 3ème colonne
+    int* cells=get_cells(t);
+    int l=4;
+    int c=2;
+    for (int j=0;j<8;j+=2){
+        if(cells[j]==0 && cells[j+1]==0){
+            mini_grille[l+cells[j]][c+cells[j+1]]='X';
+        }
+        else{
+            mini_grille[l+cells[j]][c+cells[j+1]]='O';
+        }
+    }
+    for (int m=0;m<taille;m++){
+        for(int n=0;n<taille;n++){
+            printf("%c ",mini_grille[m][n]);
+        }
+        printf("\n");
+    }
+    for(int z=0;z<taille;z++){
+        free(mini_grille[z]);
+    }
+    free(mini_grille);
+
 }
