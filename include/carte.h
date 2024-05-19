946a9734b3e5ed9b995837a2f6338bc5004f2771
Ali DAOUDI <ali.daoudi@ensiie.eu>
2024-05-19 19:24:29 +0000

    TacheE4: creer le fichier carte.h

---
+++ include/carte.h
@@ -0,0 +1,57 @@
+#ifndef __CART_H_
+#define __CART_H_
+
+/**
+ * @brief A structure representing a card.
+ */
+typedef struct s_carte* carte;
+
+/**
+ * @brief Creates a new card randomly.
+ *
+ * @return A new card.
+ * @pre A non-null card must be created.
+ * @post A new card is created randomly.
+ */
+carte create_carte();
+
+/**
+ * @brief Gets the card's number.
+ *
+ * @param cte A non-null card.
+ * @return The card's number.
+ * @pre The card must be non-null.
+ * @post Returns the card's number.
+ */
+int get_num(carte cte);
+
+/**
+ * @brief Gets the card's name.
+ *
+ * @param cte A non-null card.
+ * @return The card's name.
+ * @pre The card must be non-null.
+ * @post Returns the card's name.
+ */
+char* get_name_card(carte cte);
+
+/**
+ * @brief Retrieves the card's description or name.
+ *
+ * @param cte A non-null card.
+ * @return The card's description or name.
+ * @pre The card must be non-null.
+ * @post Returns the card's description or name.
+ */
+char* get_info_carte(carte cte);
+
+/**
+ * @brief Frees the card.
+ *
+ * @param cte A valid card address.
+ * @pre The card address must be valid.
+ * @post Frees the memory allocated to the card.
+ */
+void free_card(carte cte);
+
+#endif
