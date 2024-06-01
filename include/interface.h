b051fa2b779d39a0d0984922cf6fd138d5581e1f
Ali DAOUDI <ali.daoudi@ensiie.eu>
2024-06-01 21:29:34 +0000

    Tâche E.2/3/4:Update interface.h

--- include/interface.h
+++ include/interface.h
@@ -3,6 +3,7 @@
 // Inclusion du fichier board.h pour utiliser le type de données 'board' et 'tetromino'
 #include "board.h"  
 #include "tetromino.h"
+#include "carte.h"
 /**
  * @brief Demande à la joueuse quelle action effectuer et renvoie un chiffre correspondant.
  * 
@@ -64,55 +65,10 @@ void display_end_game(board board);
  */
 void display_message(char* message);
 
+void ask_rotate_tetromino(tetromino tetromino) ;
 
-/* E.2 */
+void display_carte(carte c);
 
-
-/**
- * @brief Demande à l'utilisateur de spécifier une rotation pour le tétrimino.
- *
- * Cette fonction affiche les options de rotation disponibles et invite l'utilisateur
- * à entrer un choix. Ensuite, elle applique la rotation choisie au tétrimino.
- *
- * @param tetromino Le tétrimino à faire pivoter.
- */
-void ask_rotate_tetromino(tetromino tetromino);
-
-
-
-
-
-
-
-/* DEBUT TACHE E4*/
-/**
- * @file cartes_operations.h
- * @brief Functions for managing special cards on the board.
- */
-
-/**
- * @brief Displays information about a specific card.
- * 
- * @param cte The card to display.
- */
-void show_card(carte cte);
-
-/**
- * @brief Executes the necessary action based on the selected card.
- * 
- * @param b A pointer to the board structure.
- * @param c The index of the card to use.
- */
-void to_need_to_use_card(board* b, int c);
-
-/**
- * @brief Asks the user if they want to use a special card and performs the action if chosen.
- * 
- * @param b A pointer to the board structure.
- */
-void ask_use_card(board* b);
-
-/*FIN de TACHE:E4 */
-
+void ask_use_carte(board b);
 
 #endif // INTERFACE_H
