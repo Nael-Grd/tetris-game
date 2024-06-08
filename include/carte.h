ba4eaffa7ac553c7179c1783f05d22dd4d8030d9
Nael GUERARD <nael.guerard@ensiie.eu>
2024-06-08 13:47:28 +0000

    Tache E.1 : récupération du ficher carte.h de lot_e sur la branche de test

--- include/carte.h
+++ include/carte.h
@@ -2,61 +2,48 @@
 #define __CART_H_
 
 /**
- * @brief A structure representing a card.
+ * @brief Structure représentant une carte.
  */
 typedef struct s_carte* carte;
 
 /**
- * @brief Creates a new card randomly.
- *
- * @return A new card.
- * @pre A non-null card must be created.
- * @post A new card is created randomly.
+ * @brief Crée une nouvelle carte avec des attributs aléatoires.
+ * 
+ * @return carte Pointeur vers la nouvelle carte créée.
  */
-carte create_carte();
+carte create_carte(void);
 
 /**
- * @brief Gets the card's number.
- *
- * @param cte A non-null card.
- * @return The card's number.
- * @pre The card must be non-null.
- * @post Returns the card's number.
+ * @brief Récupère le numéro d'une carte.
+ * 
+ * @param cte La carte dont on veut obtenir le numéro.
+ * @return int Le numéro de la carte.
  */
 int get_num(carte cte);
 
 /**
- * @brief Gets the card's name.
- *
- * @param cte A non-null card.
- * @return The card's name.
- * @pre The card must be non-null.
- * @post Returns the card's name.
+ * @brief Récupère le nom d'une carte.
+ * 
+ * @param cte La carte dont on veut obtenir le nom.
+ * @return char* Le nom de la carte.
  */
 char* get_name_card(carte cte);
 
 /**
- * @brief Retrieves the card's description or name.
- *
- * @param cte A non-null card.
- * @return The card's description or name.
- * @pre The card must be non-null.
- * @post Returns the card's description or name.
+ * @brief Récupère la description d'une carte.
+ * 
+ * @param cte La carte dont on veut obtenir la description.
+ * @return char* La description de la carte.
  */
 char* get_info_carte(carte cte);
 
 /**
- * @brief Frees the card.
- *
- * @param cte A valid card address.
- * @pre The card address must be valid.
- * @post Frees the memory allocated to the card.
+ * @brief Libère la mémoire allouée pour une carte.
+ * 
+ * @param cte La carte à libérer.
  */
 void free_card(carte cte);
 
-<<<<<<< HEAD
-#endif
-=======
 #endif /* CARTE_H */
 
->>>>>>> 63faeef75d99873448f1cdd402e62befa0e83091
+
